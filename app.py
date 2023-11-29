170adb33af40555efed7005f32d661310e473585
Tezzish <ishanpahwa14@gmail.com>
2023-11-29 11:59:52 +0100

    adding skeleton flask project

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+
+from flask import Flask, render_template, request, redirect, url_for, flash, session  
+
+app = Flask(__name__)
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 8000, debug = True)
