0396b5ed72e6371b93a61ee6f07048836a711203
Tezzish <ishanpahwa14@gmail.com>
2023-11-29 18:12:50 +0100

    getting the grid size from the frontend

--- app.py
+++ app.py
@@ -5,8 +5,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 app = Flask(__name__)
 
 @app.route('/')
-def Index():
+def index():
     return render_template('index.html')
+
+@app.route('/nonogram')
+def getRandomNonogram():
+    # Get the grid size from the URL parameters
+    grid_size = request.args.get('grid_size', '5')
+
+    print(int(grid_size) + 10)
+
+    print(f"Generating grid with size: {grid_size}")
+
+    # Return a response (for demonstration purposes)
+    return f"Generating grid with size: {grid_size}"
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8000, debug = True)
